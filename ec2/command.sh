4c23f0f63695dafaef43c5b4683495c370cdfcf5
koba-masa <kobayashi_mshr@koba-masa.com>
2021-02-14 01:04:49 +0900

    サーバ一覧を取得する関数を追加

--- ec2/command.sh
+++ ec2/command.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+source $(cd "$( dirname "$0" )" && pwd -P)/ec2/aws_cli.sh
 source $(cd "$( dirname "$0" )" && pwd -P)/ec2/utility.sh
 
 function serverlist() {
-  echo "serverlist"
+  local query="Reservations[*].Instances[*].{ServerName:Tags[?Key=='Name']|[0].Value,PrivateIpAddress:PrivateIpAddress}"
+  local filter=""
+  if [ "$1" == "--vpc" ]; then
+    filter="Name=vpc-id,Values=$(get_my_vpc_id)"
+  fi
+  describe-instances ${EXEC_TYPE_STDOUT} "${OUTPUT_TYPE_TABLE}" "${query}" "${filter}" ""
 }
