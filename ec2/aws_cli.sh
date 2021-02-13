8d1ecacb96a5812a5851175ac3cffb090c954b82
koba-masa <kobayashi_mshr@koba-masa.com>
2021-02-14 01:50:41 +0900

    タグを取得する処理を追加

--- ec2/aws_cli.sh
+++ ec2/aws_cli.sh
@@ -25,3 +25,21 @@ function describe-instances() {
       echo $(aws ec2 describe-instances ${query} --output ${output_type} ${query} ${filter} --profile ${profile})
     fi
 }
+
+function describe-tags() {
+  local exec_type=$1
+  local output_type=""
+  test "$2" != "" && output_type=$2 || output_type="table"
+  local query=""
+  test "$3" != "" && query="--query $3" || query=""
+  local filter=""
+  test "$4" != "" && filter="--filters $4" || filter=""
+  local profile=""
+  test "$5" != "" && profile=$5 || profile="${PROFILE_DEFAULT}"
+
+  if [ ${exec_type} -eq ${EXEC_TYPE_STDOUT} ]; then
+    aws ec2 describe-instances ${query} --output ${output_type} ${query} ${filter} --profile ${profile}
+  else
+    echo $(aws ec2 describe-tags ${query} --output ${output_type} ${query} ${filter} --profile ${profile})
+  fi
+}
