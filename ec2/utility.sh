f794065ab8175075c58fa673bda3a61acac467f1
koba-masa <kobayashi_mshr@koba-masa.com>
2021-02-14 01:04:22 +0900

    VPC IDを取得する関数を追加

--- ec2/utility.sh
+++ ec2/utility.sh
@@ -1,1 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+function get_my_vpc_id() {
+  local query="Reservations[*].Instances[*].NetworkInterfaces[*].VpcId"
+  local filter="Name=instance-id,Values=$(get_my_instance_id)"
+  echo `describe-instances "${EXEC_TYPE_RESULT}" "${OUTPUT_TYPE_TEXT}" "${query}" "${filter}" "${PROFILE_DEFAULT}"`
+}
+
+function get_my_instance_id() {
+  local instance_id=$(ec2-metadata -i | awk '{print $2}')
+  echo ${instance_id}
+}
